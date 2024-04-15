1fba3a78cb646f3067afd04ef5475655ff9231fe
noah <m3252@naver.com>
2024-04-15 23:06:24 +0900

    step : 도메인을 생성하고 수동 테스트를 작성

---
+++ cafekiosk/src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -0,0 +1,40 @@
+package sample.cafekiosk.unit;
+
+import sample.cafekiosk.unit.beverage.Beverage;
+import sample.cafekiosk.unit.order.Order;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CafeKiosk {
+
+    private final List<Beverage> beverages = new ArrayList<>();
+
+    public void add(Beverage beverage) {
+        beverages.add(beverage);
+    }
+
+    public void remove(Beverage beverage) {
+        beverages.remove(beverage);
+    }
+
+    public void clear() {
+        beverages.clear();
+    }
+
+    public int calculateTotalPrice() {
+        return beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
+    }
+
+    public Order createOrder() {
+        return new Order(LocalDateTime.now(), beverages);
+    }
+
+    public List<Beverage> getBeverages() {
+        return beverages;
+    }
+}
